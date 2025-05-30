bb2d1a461ef354c118f1eda530f0b22e93c2b77b
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2025-05-30 18:27:53 -0400

    added frontend basic routes and html/css files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Main from "./Pages/Main";
+
+const App = () => {
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
---
+++ client/src/Pages/Main/index.js
@@ -0,0 +1,10 @@
+
+
+const Main = () => {
+
+    return (
+        <div>Main Test Page</div>
+    )
+}
+
+export default Main;
